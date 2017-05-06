a22439a69eac97ec98ec82d992d75b9df5e89e29
JINIS-PC\JINIS <jonas@wongsrus.net>
2017-05-06 17:46:39 +1000

    Fixed the hamburger menu. It works and that's all I care about atm. Mobile app version doesn't have much to it atm.

--- js/javascript_menu.js
+++ js/javascript_menu.js
@@ -1,19 +1,31 @@
-$( document ).ready(function() {
 
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
+$(document).ready (function() {
 
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
 
+
+		$( ".cross" ).hide();
+		$( ".hamburger" ).click(function() {
+			$( ".menu" ).slideToggle( "slow", function() {
+				$( ".hamburger" ).hide();
+				$( ".cross" ).show();
+		});
+		});
+
+		$( ".cross" ).click(function() {
+			$( ".menu" ).slideToggle( "slow", function() {
+				$( ".cross" ).hide();
+				$( ".hamburger" ).show();
+			});
+		});
+		
+	$(window).resize(function(){
+	if ($('#content').width() == 320 ) {
+		$( ".menu" ).hide();
+	}
+	
 });
+	
+	
+});	
+
+$( ".menu" ).show( "slow" )
