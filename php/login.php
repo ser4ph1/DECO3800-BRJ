278b8051940ba26f0e448cb72e10c32f1f630e66
RileyDeLeacy <riley@deleacy.org>
2017-05-11 14:06:12 +1000

    Login system updated to have more fields and username displayed

--- php/login.php
+++ php/login.php
@@ -2,31 +2,35 @@
 session_start();
 include("connect.php");
 header('Content-Type: application/json');
-$username =  mysql_real_escape_string($_POST["username"]); //should be sanatised before input
+$username =  mysql_real_escape_string($_POST["username"]);
 $password = $_POST["password"]; //is crypted so doesnt need to be sanatised
-
-$query = "select password from users where username = '$username'";
+//end new
+$query = "select password from users2 where username = '$username' or email = '$username'";
 $result = $mysqli->query($query);
 $rows = mysqli_num_rows($result);
 if($rows == 0 )
 {
-	echo ("failed login");
+	//echo ("failed login");
 	$authority = false;
 }
 else
 {
-
 	while(list($passwords) = $result->fetch_row()) {
 		if(password_verify($password, $passwords)){
-			$_SESSION['username'] = $username;
-			echo json_encode(['authority' => true, 'username' => $_POST['username']]);
-				die();
+			$query = "select username from users2 where username = '$username' or email = '$username'";
+			$request = $mysqli->query($query);
+			$service = $request->fetch_assoc();
+			$_SESSION['username'] = $service['username'];
+			$authority=true;
+		
 			 //set session as logged in and redirect
 			}else{
-				echo("Failed login attempt");
+				$authority=false;
+				//echo("Failed login attempt");
 			}
 	}
 }
+echo json_encode(['authority' => $authority]);
 $mysqli->close();
 
 ?>
--- php/register.php
+++ php/register.php
@@ -3,40 +3,60 @@
 	include("connect.php");
 	header('Content-Type: application/json');
 	$username =  $mysqli->real_escape_string($_POST["usernamesignup"]);
+	$email =  $mysqli->real_escape_string($_POST["emailsignup"]);
+	$address =  $mysqli->real_escape_string($_POST["addressignup"]);
 	$password =  $mysqli->real_escape_string($_POST["passwordsignup"]);
+	$passwordconfirm =  $mysqli->real_escape_string($_POST["passwordsignupconfirm"]);
 	$validRegistration = true;
-
-	if($username == "" || $password == "")
-	{
-		//empty input
-		echo("1 or more input is blank<br>");
+	$success = false;
+	if($password!=$passwordconfirm){
 		$validRegistration = false;
 		$success = false;
 	}
-	$query = "select * from users where username = '$username'";
-	$result = $mysqli->query($query);
-	$rows = mysqli_num_rows($result);
+	if($username == "" || $password == "" || $email == "" || $address == "")
+	{
+		$validRegistration = false;
+		$success = false;
+	}
+	$query = "SELECT COUNT(*) FROM USERS2 WHERE USERNAME= ?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("s",$username);
+	$stmt->execute();
+	$stmt->bind_result($rows);
+	$stmt->fetch();
+	$stmt->close();
 	if($rows != 0)
 	{
 		// throw non unique username
-		//echo("non unique username<br>");
+		$validRegistration = false;
+		$success = false;
+	}
+	$query = "SELECT COUNT(*) FROM USERS2 WHERE EMAIL= ?";
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("s",$email);
+	$stmt->execute();
+	$stmt->bind_result($rows);
+	$stmt->fetch();
+	$stmt->close();
+	if($rows != 0)
+	{
 		$validRegistration = false;
 		$success = false;
 	}
 		
 	//account is actually registerable case
+
 	if ($validRegistration == true)
-	{
+	{	
 		$success = true;
 		$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-		$query = "INSERT INTO USERS VALUES (?, ?)";
+		$query = "INSERT INTO USERS2 VALUES (?, ?, ?, ?)";
 		$stmt = $mysqli->prepare($query);
-		$stmt->bind_param("ss", $username, $hashed_password);
+		$stmt->bind_param("ssss", $username, $email, $address, $hashed_password);
 		$stmt->execute();
 		$stmt->close();
-
 	}
-	echo json_encode(["success" => $success]);
+	$Response = array('success' => $success);
+	echo json_encode($Response);
 	$mysqli->close();
-
 ?>
